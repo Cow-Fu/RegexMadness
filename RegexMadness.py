6860d5f8b8f0a876e2cb4142beb25e68fcd3e103
Nathan Justinian <nathanj758@gmail.com>
2017-04-19 17:43:45 -0400

    crazier regex is now able to be generated

--- RegexMadness.py
+++ RegexMadness.py
@@ -1,64 +1,68 @@
-from random import choice, randint
+from random import choice, randint, randrange
 
 class FakeRegex:
 
-    def getRandomChar(self):
-        return "{}?".format(choice(self.unusedChars))
+    def getRandomSafeChar(self):
+        """Returns an alphanumeric character not at the current index of the self.word"""
+        n = self.word[self.index]
+        while True:
+            char = choice(self.safeChars)
+            if not char == n:
+                return "{}?".format(char)
 
     def getRandomMultiLetter(self):
         m = choice(self.multiLetterOptions)
         # if not m == self.multiLetterOptions[0] and randint(0, 20) < 15:
         #     return m.format(self.getRandomMultiLetter())
+        n = self.word[self.index]
         chars = ""
         for i in range(randint(1, 10)):
-            chars += choice(self.unusedChars)
+            while True:
+                char = choice(self.safeChars)
+                if not char == n:
+                    chars += char
+                    break
 
         return m.format(chars)
 
     def getRandomLookAhead(self):
-        option = choice(self.lookAhead)
-        if option == self.lookAhead[0]:     # positive lookAhead
-            return option.format(choice(self.word))
-        else:                               # negitive lookAhead
-            return option.format(choice(self.unusedChars))
+        looker = self.lookAhead[0]
+        return looker.format(choice(self.word[self.index:]))
 
     def getRandomLookBehind(self):
-        option = choice(self.lookBehind)
-        if option == self.lookBehind[0]:     # positive lookBehind
-            return option.format(choice(self.word))
-        else:                                # negitive lookBehind
-            return option.format(choice(self.unusedChars))
-
-    def insertMatch(self):
-        self.matchInserted = True
-        result = "".join(self.word)
-        self.word.pop()
-        return result
+        looker = self.lookBehind[0]
+        n = self.word[self.index]
+        while True:
+            char = choice(self.safeChars)
+            if not char == n:
+                return looker.format(char)
 
     def getAheadOrBehind(self):
-        if self.matchInserted:
+        # return self.getRandomLookAhead()
+        if self.index - 1 < 1:
+            return self.getRandomLookAhead()
+        if self.index - 1 <= self.length - 1:
             return self.getRandomLookBehind()
-        return self.getRandomLookAhead()
+        return self.getRandomLookAhead() if randrange(20) < 10 else self.getRandomLookBehind(self.index)
 
     def generate(self):
-        # options = [self.getRandomChar, self.getRandomMultiLetter, self.getAheadOrBehind]
-        options = [self.getRandomMultiLetter, self.getAheadOrBehind]
-        for i in range(101):
-            if i == 50:
-                self.regex = self.regex + self.insertMatch()
-                # print(self.insertMatch())
+        regex = ""
+        options = [self.getRandomSafeChar, self.getRandomMultiLetter, self.getAheadOrBehind]
+        while not self.index >= self.length:
+            if randrange(15) == 1:
+                regex += self.word[self.index]
+                self.index += 1
                 continue
-            self.regex = self.regex + choice(options)()
-            # print(choice(options)())
-        return self.regex
+            regex += choice(options)()
+        return regex
 
     def __init__(self, word):
-        self.regex = ""
-        self.word = word
-        self.chars = [i for i in "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"]
-        self.unusedChars = list(filter(lambda x: not x in self.word, self.chars))
-        # self.multiLetterOptions = ["[{}]?", "(?#{})"]
-        self.multiLetterOptions = ["(?#{})"]
+        self.safeChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789 "
+        self.word = list(map(lambda x: x if x in self.safeChars else "\\{}".format(x), word))
+        self.length = len(self.word)
+        self.index = 0
+        # self.unusedChars = list(filter(lambda x: not x in self.word, self.safeChars))
+        self.multiLetterOptions = ["[{}]?", "(?#{})"]
 
         self.lookAhead = [
         "(?=.*{})"   # positive
@@ -70,12 +74,6 @@ class FakeRegex:
         "(?<!{})"    # negitive
         ]
 
-        self.matchInserted = False
-
-        # print(self.getRandomChar())
-        # print(self.getRandomMultiLetter())
-        # print(self.getRandomLookAhead())
-        # print(self.getRandomLookBehind())
         print(self.generate())
 
 
